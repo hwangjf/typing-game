0496c126ad2f34ead53d20b8c9b926ad19c70b99
github username <jeffreyhwang90@gmail.com>
2018-06-19 16:20:33 -0400

    display-scores-up-to-date-refactor

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const GAMES_URL = 'http://localhost:3000/api/v1/games'
   const USERS_URL = 'http://localhost:3000/api/v1/users'
 
-  const oneStepAtTheTime = ["A Waterloo Medal was designed by sculptor Benedetto Pistrucci. Commemorating the Battle of Waterloo (18 June 1815), the medal was commissioned by the British Government in 1819 on the instructions of George IV while Prince Regent; copies were to be presented to the victorious generals and to leaders of Britain's allies. The Prince Regent and William Wellesley-Pole, Master of the Mint, had been impressed by Pistrucci's models, and gave him the commission."]
+  const oneStepAtTheTime = [
+    "A Waterloo Medal was designed by sculptor Benedetto Pistrucci. Commemorating the Battle of Waterloo (18 June 1815), the medal was commissioned by the British Government in 1819 on the instructions of George IV while Prince Regent; copies were to be presented to the victorious generals and to leaders of Britain's allies. The Prince Regent and William Wellesley-Pole, Master of the Mint, had been impressed by Pistrucci's models, and gave him the commission.",
+    `Augusta Ada King-Noel, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognize that the machine had applications beyond pure calculation, and published the first algorithm intended to be carried out by such a machine. As a result, she is sometimes regarded as the first to recognize the full potential of a "computing machine" and the first computer programmer.`,
+    `JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web. JavaScript enables interactive web pages and thus is an essential part of web applications. The vast majority of websites use it, and all major web browsers have a dedicated JavaScript engine to execute it.`,
+    `Matsumoto stresses that systems design needs to emphasize human, rather than computer, needs: Often people, especially computer engineers, focus on the machines. They think, "By doing this, the machine will run fast. By doing this, the machine will run more effectively. By doing this, the machine will something something something." They are focusing on machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines. We are the masters. They are the slaves.`,
+    `A space elevator is a proposed type of planet-to-space transportation system.[1] The main component would be a cable (also called a tether) anchored to the surface and extending into space. The design would permit vehicles to travel along the cable from a planetary surface, such as the Earth's, directly into space or orbit, without the use of large rockets. An Earth-based space elevator would consist of a cable with one end attached to the surface near the equator and the other end in space beyond geostationary orbit (35,786 km altitude). The competing forces of gravity, which is stronger at the lower end, and the outward/upward centrifugal force, which is stronger at the upper end, would result in the cable being held up, under tension, and stationary over a single position on Earth. With the tether deployed, climbers could repeatedly climb the tether to space by mechanical means, releasing their cargo to orbit. Climbers could also descend the tether to return cargo to the surface from orbit.`,
+    `Alan Mathison Turing was an English computer scientist, mathematician, logician, cryptanalyst, philosopher, and theoretical biologist. Turing was highly influential in the development of theoretical computer science, providing a formalisation of the concepts of algorithm and computation with the Turing machine, which can be considered a model of a general purpose computer. Turing is widely considered to be the father of theoretical computer science and artificial intelligence.`,
+    `Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American entrepreneur and business magnate. He was the chairman, chief executive officer (CEO), and a co-founder of Apple Inc., chairman and majority shareholder of Pixar, a member of The Walt Disney Company's board of directors following its acquisition of Pixar, and the founder, chairman, and CEO of NeXT. Jobs and Apple co-founder Steve Wozniak are widely recognized as pioneers of the microcomputer revolution of the 1970s and 1980s.`,
+    `Mammals (class Mammalia) are vertebrate animals characterized by the presence of sweat glands, including milk producing sweat glands, and by the presence of: hair, three middle ear bones used in hearing, and a neocortex region in the brain. Mammals, other than the monotremes, give birth to live young instead of laying eggs. They also possess specialized teeth and use a placenta in the ontogeny. The mammalian brain regulates endothermic and circulatory systems, including a four-chambered heart. Mammals encompass approximately 5,400 species, ranging in size from the Bumblebee Bat, (30-40mm), to the Blue Whale, (33,000mm), distributed in about 1,200 genera, 153 families, and 29 orders, though this varies by classification scheme.`
+  ]
 
   const container = document.getElementById("container");
   const leaderBoard = document.getElementById('leaderboard');
@@ -20,26 +29,33 @@ document.addEventListener("DOMContentLoaded", function() {
   function requestGames() {
     let h1 = document.createElement('H1')
     h1.innerText = "Leaderboard"
-    
+
     leaderBoard.appendChild(h1)
 
     fetch(GAMES_URL).then(response=>response.json()).then(displayGames)
   }
 
   function displayGames(gameObjs) {
+    let ol = document.createElement('OL')
+
     usersGetRequest().then(userObjs =>
       userObjs.forEach(function(userObj) {
         usersArray.push(userObj);
       })
     ).then(() => {
-      gameObjs.forEach(gameObj => {
+      let gamesArray = Array.from(gameObjs).sort(function(a,b) {
+        return parseInt(b.wpm)*parseInt(b.accuracy)-parseInt(a.wpm)*parseInt(a.accuracy)
+      })
+
+      gamesArray.forEach(gameObj => {
         let userInstance = usersArray.find(user => user.id === gameObj.user_id)
         let li = document.createElement('LI')
-        li.innerText = `${userInstance.name} | ${gameObj.accuracy} | ${gameObj.wpm} wpm`
+        li.innerText = `${userInstance.name} | Accuracy: ${gameObj.accuracy} | WPM: ${gameObj.wpm} | Score: ${parseInt(gameObj.accuracy)*parseInt(gameObj.wpm)}`
 
-        leaderBoard.appendChild(li)
+        ol.appendChild(li)
       })
     })
+    leaderBoard.appendChild(ol)
   }
 
   function getUserName() {
@@ -53,9 +69,6 @@ document.addEventListener("DOMContentLoaded", function() {
     return fetch(USERS_URL).then(resp=>resp.json())
   }
 
-  usersGetRequest()
-  // function
-
   function userPostRequest(e) {
     // e.preventDefault()
     let nameInput = document.getElementById('name-input-field').value
@@ -65,15 +78,12 @@ document.addEventListener("DOMContentLoaded", function() {
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({name: nameInput})
     }
-
-    fetch('http://0.0.0.0:3000/api/v1/users', config).then(resp=>resp.json()).then(data => user = data).then(pageSetUp)
-
+    fetch(USERS_URL, config).then(resp=>resp.json()).then(data => user = data).then(pageSetUp)
   }
 
   function pageSetUp() {
-    // console.log(user)
-
     container.innerHTML = ""
+    leaderBoard.innerHTML = ""
     if (document.getElementById('clock')) {
       document.getElementById('clock').remove();
     }
@@ -84,115 +94,96 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function startClock() {
-       intervalFn = setInterval(handleInterval, 1000);
-       setTimeout(stopClock, 10000)
+    intervalFn = setInterval(handleInterval, 1000);
+    setTimeout(stopClock, 10000)
   }
 
   function handleInterval() {
-      const clockDiv = document.getElementById("clock")
-      clockDiv.innerText = --timerCount;
-
+    const clockDiv = document.getElementById("clock")
+    clockDiv.innerText = --timerCount;
   }
 
   function disableInterval() {
-      clearInterval(intervalFn);
+    clearInterval(intervalFn);
   }
 
   function displayClock(){
-      let clockDiv = document.createElement("div");
-      clockDiv.id = "clock";
-      clockDiv.style.backgroundColor = "black";
-      clockDiv.style.margin = "auto";
-      clockDiv.style.fontSize = '50px';
-      clockDiv.style.color = 'white';
+    let clockDiv = document.createElement("div");
+    clockDiv.id = "clock";
+    clockDiv.style.backgroundColor = "black";
+    clockDiv.style.margin = "auto";
+    clockDiv.style.fontSize = '50px';
+    clockDiv.style.color = 'white';
 
-      clockDiv.style.width = '100px';
-      clockDiv.style.height = '60px';
-      clockDiv.style.textAlign = "center";
+    clockDiv.style.width = '100px';
+    clockDiv.style.height = '60px';
+    clockDiv.style.textAlign = "center";
 
-      clockDiv.innerText = timerCount;
-      document.body.prepend(clockDiv);
-
+    clockDiv.innerText = timerCount;
+    document.body.prepend(clockDiv);
   }
 
-
   function displayText() {
-      container.innerText = oneStepAtTheTime[0];
-      container.innerHTML += "<br>";
-      let inputForm = document.createElement("textarea");
-      inputForm.type = 'text';
-      // inputForm.rows = "6";
-      // inputForm.cols = "120"
-      inputForm.style.width = "100%";
-      inputForm.style.height = "100px";
-      inputForm.style.fontSize = "20px";
-      // inputForm.style.alignSelf = "center";
-      inputForm.id = "inputTxt";
-      container.appendChild(inputForm);
+    container.innerText = oneStepAtTheTime[Math.floor(Math.random()*oneStepAtTheTime.length)];
+    container.innerHTML += "<br>";
+    let inputForm = document.createElement("textarea");
+    inputForm.type = 'text';
+    inputForm.style.width = "100%";
+    inputForm.style.height = "100px";
+    inputForm.style.fontSize = "20px";
+    inputForm.id = "inputTxt";
+    container.appendChild(inputForm);
 
+    let displayAcc = document.createElement("div");
+    displayAcc.id = "display-accuracy";
+    container.appendChild(displayAcc);
 
-      let displayAcc = document.createElement("div");
-      displayAcc.id = "display-accuracy";
-      container.appendChild(displayAcc);
+    document.addEventListener('keydown', event => {
+      if(event.target.tagName === "TEXTAREA" && flag === true){
+        startClock(event);
+        flag = false;
+      }
+    });// document.addEventListener
 
-
-      document.addEventListener('keydown', event => {
-          if(event.target.tagName === "TEXTAREA" && flag === true){
-              startClock(event);
-              flag = false;
-          }
-          });// document.addEventListener
-
-
-      inputForm.addEventListener('keyup', event => {
-          if ((event.which <= 90 && event.which >= 48) || (event.which <= 222 && event.which >= 186) || event.which === 32 ){
-              counterKeystroke ++;
-              startTest(event,displayAcc);
-          }else{ console.log(event.target)}
-      })//inputForm.addEventListener
-
+    inputForm.addEventListener('keyup', event => {
+      if ((event.which <= 90 && event.which >= 48) || (event.which <= 222 && event.which >= 186) || event.which === 32 ){
+        counterKeystroke ++;
+        startTest(event,displayAcc);
+      } else {console.log(event.target)}
+    })//inputForm.addEventListener
   }
 
   function startTest(event,displayAcc) {
-      let currentText = event.target.value;
-      let index = currentText.length -1;
-      // debugger;
-      // console.log(timerCount);
+    let currentText = event.target.value;
+    let index = currentText.length -1;
 
-
-      if (container.innerText[index] === currentText[index]){
-          document.body.style.backgroundColor = 'green';
-          correctStrokeCnt ++;
-      }else{
-          document.body.style.backgroundColor = 'red';
-      }
-      // console.log(counterKeystroke);
-      // console.log(correctStrokeCnt);
-
-      displayAcc.innerText = `${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%`;
-
+    if (container.innerText[index] === currentText[index]){
+      document.body.style.backgroundColor = 'green';
+      correctStrokeCnt ++;
+    } else {
+      document.body.style.backgroundColor = 'red';
+    }
+    displayAcc.innerText = `Accuracy: ${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%`;
   }
 
+
+
   function stopClock() {
-        // if (timerCount < 1){
-        let inputForm = document.getElementById('inputTxt')
-        const clockDiv = document.getElementById("clock")
-        // timerCount = 0;
-        clockDiv.innerText = 0;
-        let wordArray = inputForm.value.trim().split(' ')
-        if (clockDiv.innerText == 0) {
-          endOfGameAlert(wordArray)
-        }
-        // event.target.disabled = true;
-        disableInterval();
-      // }
+    let inputForm = document.getElementById('inputTxt')
+    const clockDiv = document.getElementById("clock")
+
+    clockDiv.innerText = 0;
+    let wordArray = inputForm.value.trim().split(' ')
+    if (clockDiv.innerText == 0) {
+      endOfGameAlert(wordArray)
+    }
+    disableInterval();
   }
 
   function endOfGameAlert(wordArray) {
-    // alert(`Time out! Your accuracy is ${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%, and you typed ${wordArray.length * 2} words per minute.`)
     let typingAccuracy = `${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%`
     let wordsPerMin = `${wordArray.length * 6}`
-    container.innerHTML += `<div><h3>Game over! Your accuracy is ${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%, and you typed ${wordArray.length * 6} words per minute.</h3><button id="play-again">Play again?</button></div>`
+    container.innerHTML += `<div><h3>Game over! Your accuracy is ${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%, and you typed ${wordArray.length * 6} words per minute. Your score is ${parseInt(typingAccuracy)*parseInt(wordsPerMin)}.</h3><button id="play-again">Play again?</button></div>`
 
     let playAgainButton = document.getElementById('play-again')
     playAgainButton.addEventListener('click', pageSetUp)
@@ -207,7 +198,4 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   getUserName()
-  // pageSetUp()
-
-
 })

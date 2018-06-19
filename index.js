47c0170259d2de1002f1845345721af056c0117b
ensallee <ensallee@gmail.com>
2018-06-19 11:18:53 -0400

    added user prompt

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const oneStepAtTheTime = ["A Waterloo Medal was designed by sculptor Benedetto Pistrucci. Commemorating the Battle of Waterloo (18 June 1815), the medal was commissioned by the British Government in 1819 on the instructions of George IV while Prince Regent; copies were to be presented to the victorious generals and to leaders of Britain's allies. The Prince Regent and William Wellesley-Pole, Master of the Mint, had been impressed by Pistrucci's models, and gave him the commission."]
 
@@ -10,7 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let timerCount = 10;
     let flag = true;
+    let user;
 
+
+
+    function getUserName() {
+      container.innerHTML += `Please Enter Your Name: <input id="name-input-field" type="text">
+      <button id="name-submit" type="submit">Submit</button>`
+      let submitButton = document.getElementById('name-submit')
+      submitButton.addEventListener('click', userPostRequest)
+    }
+
+    function userPostRequest(e) {
+      // e.preventDefault()
+      let nameInput = document.getElementById('name-input-field').value
+
+      let config = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name: nameInput})
+      }
+
+      fetch('http://0.0.0.0:3000/api/v1/users', config).then(resp=>resp.json()).then(data => user = data).then(pageSetUp)
+
+    }
+
+    function pageSetUp() {
+      console.log(user)
+      container.innerHTML = ""
+      displayClock();
+      displayText();
+    }
 
     function startClock() {
          intervalFn = setInterval(handleInterval, 1000);
@@ -45,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function displayText() {
         container.innerText = oneStepAtTheTime[0];
-        container.innerHTML += "<br>"; 
+        container.innerHTML += "<br>";
         let inputForm = document.createElement("textarea");
         inputForm.type = 'text';
         // inputForm.rows = "6";
@@ -62,7 +91,7 @@ document.addEventListener("DOMContentLoaded", function() {
         displayAcc.id = "display-accuracy";
         container.appendChild(displayAcc);
 
-        
+
         document.addEventListener('keydown', event => {
             if(event.target.tagName === "TEXTAREA" && flag === true){
                 startClock(event);
@@ -105,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function() {
         displayAcc.innerText = `${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%`;
 
     }
-    displayClock();
-    displayText();
+
+    getUserName()
 
 })

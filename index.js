a590efb40765ea10f00651f6e944a30e800a1b38
Derek Chen <derekgc@icloud.com>
2018-06-20 10:32:13 -0400

    fix the highlight

--- index.js
+++ index.js
@@ -1,183 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const GAMES_URL = 'http://localhost:3000/api/v1/games'
   const USERS_URL = 'http://localhost:3000/api/v1/users'
-  let capitalize
-  let toCapitalize
 
-  const keyCodes = {
-  0: 'That key has no keycode',
-  3: 'break',
-  8: 'backspace / delete',
-  9: 'tab',
-  12: 'clear',
-  13: 'enter',
-  16: 'shift',
-  17: 'ctrl',
-  18: 'alt',
-  19: 'pause/break',
-  20: 'caps lock',
-  21: 'hangul',
-  25: 'hanja',
-  27: 'escape',
-  28: 'conversion',
-  29: 'non-conversion',
-  32: 'spacebar',
-  33: 'page up',
-  34: 'page down',
-  35: 'end',
-  36: 'home',
-  37: 'left arrow',
-  38: 'up arrow',
-  39: 'right arrow',
-  40: 'down arrow',
-  41: 'select',
-  42: 'print',
-  43: 'execute',
-  44: 'Print Screen',
-  45: 'insert',
-  46: 'delete',
-  47: 'help',
-  48: '0',
-  49: '1',
-  50: '2',
-  51: '3',
-  52: '4',
-  53: '5',
-  54: '6',
-  55: '7',
-  56: '8',
-  57: '9',
-  58: ':',
-  59: 'semicolon (firefox), equals',
-  60: '<',
-  61: 'equals (firefox)',
-  63: 'ß',
-  64: '@ (firefox)',
-  65: 'a',
-  66: 'b',
-  67: 'c',
-  68: 'd',
-  69: 'e',
-  70: 'f',
-  71: 'g',
-  72: 'h',
-  73: 'i',
-  74: 'j',
-  75: 'k',
-  76: 'l',
-  77: 'm',
-  78: 'n',
-  79: 'o',
-  80: 'p',
-  81: 'q',
-  82: 'r',
-  83: 's',
-  84: 't',
-  85: 'u',
-  86: 'v',
-  87: 'w',
-  88: 'x',
-  89: 'y',
-  90: 'z',
-  91: 'Windows Key / Left ⌘ / Chromebook Search key',
-  92: 'right window key',
-  93: 'Windows Menu / Right ⌘',
-  95: 'sleep',
-  96: 'numpad 0',
-  97: 'numpad 1',
-  98: 'numpad 2',
-  99: 'numpad 3',
-  100: 'numpad 4',
-  101: 'numpad 5',
-  102: 'numpad 6',
-  103: 'numpad 7',
-  104: 'numpad 8',
-  105: 'numpad 9',
-  106: 'multiply',
-  107: 'add',
-  108: 'numpad period (firefox)',
-  109: 'subtract',
-  110: 'decimal point',
-  111: 'divide',
-  112: 'f1',
-  113: 'f2',
-  114: 'f3',
-  115: 'f4',
-  116: 'f5',
-  117: 'f6',
-  118: 'f7',
-  119: 'f8',
-  120: 'f9',
-  121: 'f10',
-  122: 'f11',
-  123: 'f12',
-  124: 'f13',
-  125: 'f14',
-  126: 'f15',
-  127: 'f16',
-  128: 'f17',
-  129: 'f18',
-  130: 'f19',
-  131: 'f20',
-  132: 'f21',
-  133: 'f22',
-  134: 'f23',
-  135: 'f24',
-  144: 'num lock',
-  145: 'scroll lock',
-  160: '^',
-  161: '!',
-  162: '؛ (arabic semicolon)',
-  163: '#',
-  164: '$',
-  165: 'ù',
-  166: 'page backward',
-  167: 'page forward',
-  168: 'refresh',
-  169: 'closing paren (AZERTY)',
-  170: '*',
-  171: '~ + * key',
-  172: 'home key',
-  173: 'minus (firefox), mute/unmute',
-  174: 'decrease volume level',
-  175: 'increase volume level',
-  176: 'next',
-  177: 'previous',
-  178: 'stop',
-  179: 'play/pause',
-  180: 'e-mail',
-  181: 'mute/unmute (firefox)',
-  182: 'decrease volume level (firefox)',
-  183: 'increase volume level (firefox)',
-  186: 'semi-colon / ñ',
-  187: 'equal sign',
-  188: ',',
-  189: '-',
-  190: '.',
-  191: 'forward slash / ç',
-  192: 'grave accent / ñ / æ / ö',
-  193: '?, / or °',
-  194: 'numpad period (chrome)',
-  219: 'open bracket',
-  220: 'back slash',
-  221: 'close bracket / å',
-  222: 'single quote / ø / ä',
-  223: '`',
-  224: 'left or right ⌘ key (firefox)',
-  225: 'altgr',
-  226: '< /git >, left back slash',
-  230: 'GNOME Compose Key',
-  231: 'ç',
-  233: 'XF86Forward',
-  234: 'XF86Back',
-  235: 'non-conversion',
-  240: 'alphanumeric',
-  242: 'hiragana/katakana',
-  243: 'half-width/full-width',
-  244: 'kanji',
-  255: 'toggle touchpad',
-};
 
+ 
   const oneStepAtTheTime = [
     "A Waterloo Medal was designed by sculptor Benedetto Pistrucci. Commemorating the Battle of Waterloo (18 June 1815), the medal was commissioned by the British Government in 1819 on the instructions of George IV while Prince Regent; copies were to be presented to the victorious generals and to leaders of Britain's allies. The Prince Regent and William Wellesley-Pole, Master of the Mint, had been impressed by Pistrucci's models, and gave him the commission.",
     `Augusta Ada King-Noel, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognize that the machine had applications beyond pure calculation, and published the first algorithm intended to be carried out by such a machine. As a result, she is sometimes regarded as the first to recognize the full potential of a "computing machine" and the first computer programmer.`,
@@ -335,11 +161,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let pArray = Array.from(document.getElementsByTagName('P')[0].children)
 
-    inputForm.addEventListener('keydown', event => {
-      if (event.which === 16 ) {
-        capitalize = true
-      }
-      console.log(`which is `, event.which)
+    inputForm.addEventListener('keyup', event => {
+
+    //   console.log(`which is `, event.which)
       if ((event.which <= 90 && event.which >= 48) || (event.which <= 222 && event.which >= 186) || event.which === 32 ){
         counterKeystroke ++;
         startTest(event,displayAcc,pArray);
@@ -348,37 +172,51 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function startTest(event,displayAcc,p) {
-    let insertedLetter
-    if (capitalize) {
-      insertedLetter = keyCodes[event.which]
-      insertedLetter = insertedLetter.toUpperCase()
-      capitalize = false
-    } else {
-        insertedLetter = keyCodes[event.which]
-    }
+
     let currentText = event.target.value;
-    console.log(`inserted letter ${insertedLetter}`)
-    let index = currentText.length;
+
+    // let index = currentText.length -1;
     //console.log(p)
-    console.log(`The inserted letter is ${currentText[index]}`)
-    console.log(`The letter above is ${p[index].innerText}`)
+
     //debugger
     // p[index].innerText === currentText[index]
 
-    if (p[index].innerText === insertedLetter){
-      console.log(`The letter matches`)
-      p[index].style.color = 'green';
-      p[index].style.fontSize = '40px';
-      correctStrokeCnt++;
-    } else {
-      console.log(`The letter did not matche`)
-      p[index].style.color = 'red';
-      p[index].style.fontSize = '40px';
-    }
+    // if (p[index].innerText === currentText[index]){
+    //   console.log(`The letter matches`)
+    //   p[index].style.color = 'green';
+    //   p[index].style.fontSize = '40px';
+    //   correctStrokeCnt++;
+    // } 
+    // else {
+    // //   console.log(`The letter did not matche`)
+    //   p[index].style.color = 'red';
+    //   p[index].style.fontSize = '40px';
+    // }
+
+    correctStrokeCnt = checkTyping(currentText, p);
+
+
     displayAcc.innerText = `Accuracy: ${parseFloat((correctStrokeCnt/counterKeystroke)*100).toFixed(2)}%`;
   }
 
 
+  function checkTyping(currentText, p) {
+      let index =0;
+      let counter = 0;
+    [...currentText].forEach(e => {
+        if (p[index].innerText === e){
+            p[index].style.color = 'green';
+            p[index].style.fontSize = '40px';
+            counter ++;
+          } else {
+            p[index].style.color = 'red';
+            p[index].style.fontSize = '40px';
+          }
+        index ++;
+    })
+
+    return counter;
+  }
 
   function stopClock() {
     let inputForm = document.getElementById('inputTxt')
